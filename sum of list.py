741041b884541c9fc6639909b8e65410e9547a6c
poojashree7 <poojashreeb297@gmail.com>
2025-09-10 17:48:20 +0530

    Add files via upload

---
+++ sum of list.py
@@ -0,0 +1,6 @@
+a=[5,10,15,20]
+b=[11,22,33,44]
+print("sum of 5 and 11 is:", a[0]+b[0])
+print("sum of 10 and 22 is:", a[1]+b[1])
+print("sum of 15 and 33 is:", a[2]+b[2])
+print("sum of 20 and 44 is:", a[3]+b[3])
